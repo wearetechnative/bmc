b2a0da1c0aa0690368873f461aab48a638ce34f8
Wouter van der Toorren <githubwto@toorren.net>
2024-01-31 07:56:32 +0100

    work with tfvars files different than backend

--- tfplan.sh
+++ tfplan.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
+# Version: 2024013101
 
-TF_ENV=$(echo $TF_BACKEND |awk -F '.' '{print $1}')
+TF_ENV=$(echo $TF_BACKEND | awk 0-F '.' '{print $1}' 2>&1)
+TF_VARS=$(find . -type f -name "*.tfvars")
+
 
 if [[ -f ${TF_ENV}.tfvars ]]; then
     terraform plan -var-file=${TF_ENV}.tfvars $@
-else
-    terraform plan $@
-fi
+elif [[ ! -z ${TF_VARS} ]]; then
 
+    TF_VARS=(${TF_VARS})
+    TF_VARS_len=${#TF_VARS[*]}
+
+
+    echo "--- Choose vars-file"
+    echo "-: Quit"
+    for ((i = 0; i < $TF_VARS_len; i++)); do
+        echo "$i: ${TF_VARS[$i]}"
+    done
+    echo
+    printf 'Selection: '
+    read choice
+    COLUMN=6
+    case $choice in
+    '' | *[!0-9\-]*)
+        clear
+        echo Invalid selection. Make a valid selection from the list above or press ctrl+c to exit
+        echo '-> Error: Not a number, and not "-"'
+        echo
+        break
+        ;;
+    esac
+    in_range=false
+    while [ $in_range != true ]; do
+        if [[ $choice == '-' ]]; then
+            break
+        elif (($choice >= 0)) && (($choice <= (${TF_VARS_len}-1))); then
+
+            echo ${TF_VARS[choice]}
+            in_range=true
+            terraform plan -var-file=${TF_VARS[choice]} $@
+            break
+        else
+            echo "!! Not a valid option"
+            break
+        fi
+    done
+
+fi
