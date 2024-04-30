829432425e719fde0cbefb28c3a3297a34b2df0e
Wouter van der Toorren <githubwto@toorren.net>
2024-04-30 15:00:22 +0200

    set comments

--- ecsconnect.sh
+++ ecsconnect.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+# W. van der Toorren - TechNative B>V
+# version: 2024043001
 #
 
+# Init variables
+breadcrumbs=""
+
 print_breadcrumbs() {
+  ## Function to print breadcrumbs, to give some information about the choices
   printf "\r%s\n" "$1"
 }
 
-# Initialize breadcrumbs variable
-breadcrumbs=""
 
 # Function to update breadcrumbs
 update_breadcrumbs() {
@@ -18,28 +22,29 @@ update_breadcrumbs() {
   print_breadcrumbs "$breadcrumbs"
 }
 
-while getopts 'i:' opt; do
-  case "$opt" in
-  i)
-    sshKey="$OPTARG"
-    if [[ -f ${sshKey} ]]; then
-      echo "Using ssh-key: '${OPTARG}' "
-      sshKey="-i ${OPTARG}"
-    else
-      echo "ssh-key not found: '${OPTARG}'. Not using it"
-      unset sshKey
-    fi
-    ;;
+# #Menu to pass ssh-k
+# while getopts 'i:' opt; do
+#   case "$opt" in
+#   i)
+#     sshKey="$OPTARG"
+#     if [[ -f ${sshKey} ]]; then
+#       echo "Using ssh-key: '${OPTARG}' "
+#       sshKey="-i ${OPTARG}"
+#     else
+#       echo "ssh-key not found: '${OPTARG}'. Not using it"
+#       unset sshKey
+#     fi
+#     ;;
 
-  ?)
-    echo -e "Invalid command option.\nUsage: $(basename $0) [-i path_to/ssh_key]"
-    exit 1
-    ;;
-  esac
-done
-shift "$(($OPTIND - 1))"
+#   ?)
+#     echo -e "Invalid command option.\nUsage: $(basename $0) [-i path_to/ssh_key]"
+#     exit 1
+#     ;;
+#   esac
+# done
+# shift "$(($OPTIND - 1))"
 
-# Check AWS credentials via AWS_PROFILE and AWS_DEFAULT_REGION
+# Check AWS credentials via AWS_PROFILE
 if [[ -z ${AWS_PROFILE} ]]; then
   echo "!! AWS_PROFILE  not set"
   exit 1
@@ -50,7 +55,7 @@ fi
 #   exit 1
 # fi
 
-# execute AWS CLI-command to retrieve info about EC2-instances. Exit when command fails
+# execute AWS CLI-command to retrieve info about clusters. Exit when command fails
 aws_output=$(aws ecs list-clusters 2>/dev/null)
 if [[ $? -ne 0 ]]; then
   echo "!! Error listing clusters. Check AWS credentials"
@@ -61,42 +66,44 @@ fi
 echo "-- Select cluster"
 clusters=$(echo "$aws_output" | jq -r '.clusterArns[] | split("/")[-1]')
 cluster=$(gum choose $clusters)
-update_breadcrumbs "$cluster"
-clustertype=$(aws ecs describe-clusters --cluster ${cluster} | jq -r '.clusters[0].capacityProviders[]')
+update_breadcrumbs "$cluster" # update breadcrumbs with clustername
+# clustertype=$(aws ecs describe-clusters --cluster ${cluster} | jq -r '.clusters[0].capacityProviders[]')
 
-  aws_output=$(aws ecs list-services --cluster ${cluster} 2>/dev/null)
-  if [[ $? -ne 0 ]]; then
-    echo "!! Error listing services"
-    exit 1
-  fi
+# execute AWS CLI-command to retrieve info about services in the cluster. Exit when command fails
+aws_output=$(aws ecs list-services --cluster ${cluster} 2>/dev/null)
+if [[ $? -ne 0 ]]; then
+  echo "!! Error listing services"
+  exit 1
+fi
 
-  echo "-- Select service"
-  services=$(echo "$aws_output" | jq -r '.serviceArns[] | split("/")[-1]')
-  service=$(gum choose $services)
-  update_breadcrumbs "$service"
+echo "-- Select service"
+services=$(echo "$aws_output" | jq -r '.serviceArns[] | split("/")[-1]')
+service=$(gum choose $services)
+update_breadcrumbs "$service" # update breadcrumbs with clustername
 
-  aws_output=$(aws ecs list-tasks --cluster ${cluster} --service ${service})
-  if [[ $? -ne 0 ]]; then
-    echo "!! Error listing tasks."
-    exit 1
-  fi
+# execute AWS CLI-command to retrieve info about tasks in the service. Exit when command fails
+aws_output=$(aws ecs list-tasks --cluster $cluster --service $service --desired-status RUNNING --query 'taskArns[]')
+if [[ $? -ne 0 ]]; then
+  echo "!! Error listing tasks."
+  exit 1
+fi
 
-  echo "-- Select task_id"
-  taskids=$(echo "$aws_output" | jq -r '.taskArns[] | split("/")[-1]')
-  taskid=$(gum choose $taskids)
+echo "-- Select task_id"
+# Filter taskArns based on lastStatus
+taskids=$(echo "$aws_output" |jq -r '.[] | split("/") | last')
+taskid=$(gum choose $taskids)
+update_breadcrumbs "$taskid"# update breadcrumbs with clustername
 
-  update_breadcrumbs "$taskid"
+aws_output=$(aws ecs describe-tasks --cluster ${cluster} --tasks ${taskids})
+if [[ $? -ne 0 ]]; then
+  echo "!! Error listing containers. Check AWS credentials"
+  exit 1
+fi
 
-  aws_output=$(aws ecs describe-tasks --cluster ${cluster} --tasks ${taskids})
-  if [[ $? -ne 0 ]]; then
-    echo "!! Error listing containers. Check AWS credentials"
-    exit 1
-  fi
+containers=$(echo "$aws_output" | jq -r '.tasks[0].containers[].name')
+echo "-- Select container"
+container=$(gum choose $containers)
+taskarn=$(echo "$aws_output" | jq -r '.tasks[0].taskArn')
+update_breadcrumbs "$container"
 
-  echo "-- Select container"
-  containers=$(echo "$aws_output" | jq -r '.tasks[0].containers[].name')
-  container=$(gum choose $containers)
-  taskarn=$(echo "$aws_output" | jq -r '.tasks[0].taskArn')
-  update_breadcrumbs "$container"
-
-  aws ecs execute-command --cluster ${cluster} --interactive --container ${container} --command /bin/bash --task ${taskarn}
+aws ecs execute-command --cluster ${cluster} --interactive --container ${container} --command /bin/bash --task ${taskarn}
