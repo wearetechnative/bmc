19b5e9303aea3c60bb5205b84aab808a3137f002
TorreiroW <githubwto@toorren.net>
2024-09-06 19:40:26 +0200

    revision of aws profile switch

--- aws-profile-select.sh
+++ aws-profile-select.sh
@@ -1,258 +1,105 @@
 #!/usr/bin/env bash
-# Version: 202407231
+
+## User Variables
+MFA=true
+SSO=""
+totpScript=""
 
 
-rprompt_config="true"
-aws_sso="false"
-aws_mfa="false"
+function setDates {
+  unset currentMFASessionExpirationDate
+  expiration=$(sed -n -e "/\[$sourceProfile\]/,/^$/ s/^[[:space:]]*expiration[[:space:]]*=[[:space:]]*\(.*\)/\1/p" "$HOME/.aws/credentials")
+  if [[ -z ${expiration} ]]; then expiration="1970-01-01 01:00:00"; fi
 
-totpScript=""  # example bitwarden: "rbw-menu.sh -t code -q new"
-clipboardCommand="" # examplke linux xclip: "xclip -selection clipboard"
 
-mkdir -p ~/.config/aws-profile-select/
-
-if [[ -f ~/.config/aws-profile-select/env ]]; then
-  source ~/.config/aws-profile-select/env
-  echo "MFA enabled"
-fi
-
-# Enable setting of AWS_SDK_LOAD_CONFIG by default
-sdk=1
-exit_script="false"
-unset mfaduration
-
-if [ -n "$ZSH_VERSION" ]; then
-  # zsh-handling
-  shell_type=zsh
-  # echo "!! check zsh_version"
-  setopt ksh_arrays
-  setopt SH_WORD_SPLIT
-  if [ -z "$PROMPTBAK" ]; then
-    export PROMPTBAK="$PROMPT"
-    cmd_prompt="$PROMPT"
+  if [[ ${osType} == "macos" ]]; then
+    currentMFASessionExpirationDate=$(date -j -f "%Y-%m-%d %H:%M:%S" "${expiration}" "+%s" 2>/dev/null)
+    dateCmd="date -j -f "
+  elif [[ ${osType} == "linux" ]]; then
+    currentMFASessionExpirationDate=$(date -d "$expiration" +%s 2>/dev/null)
   else
-    cmd_prompt="$PROMPTBAK"
+    currentMFASessionExpirationDate="0"
   fi
-else
-  # bash-handling
-  # Backup prompt as a separate variable if not already backed up
-  if [ -z "$PS1BAK" ]; then
-    export PS1BAK="$PS1"
-    cmd_prompt="$PS1"
-  else
-    cmd_prompt="$PS1BAK"
-  fi
-fi
-
-profiles=$(grep '^\[' <~/.aws/config | sed -E 's/\[profile (.*)/\1/g' | sed 's/\[//; s/\]//')
-
-IFSBAK=$IFS
-IFS=$'\n'
-profiles=(${profiles})
-IFS=$IFSBAK
-
-profiles_len=${#profiles[*]}
-
-function main {
-  # parse_arguments
-  printf "Current value of AWS_SDK_LOAD_CONFIG: ${AWS_SDK_LOAD_CONFIG}\n"
-  echo ------------- AWS Profile Select-O-Matic -------------
-  if [ -z "$AWS_PROFILE" ]; then
-    printf "No profile set yet\n\n"
-  else
-    printf "\nCurrently-selected profile: ${AWS_PROFILE}\n\n"
-  fi
-  echo "Type the number of the profile you want to use from the list below, and press enter"
-  echo
-
-  # Show the menu
-  # echo "!! READ SELECTION"
-  read_selection
-  # echo "!! CHECK DSK"
-  check_sdk
-  # echo "!! SET PROMPT"
-
-  if [[ ${aws_mfa} == "true" ]]; then mfa; fi
-  # if [[ ${aws_sso} == "true" ]]; then sso; fi
-
+  date_now=$(date +%s)
 }
 
-function usage {
-  echo "Usage: aps [-n|--no-sdk] [-h|--help]"
-  echo "For normal usage, just run aps and make your selection followed by Enter."
-  echo "If you do not want the AWS_SDK_LOAD_CONFIG environment variable to be set to true, append -n or --no-sdk to the command"
-  # exit 1
-}
 
 function checkOS {
   if [ -f /etc/lsb-release ]; then
-    echo "linux"
+    osType="linux"
   elif [ -f /System/Library/CoreServices/SystemVersion.plist ]; then
-    echo "macos"
+    osType="macos"
   else
-    echo "other"
+    osType="other"
   fi
-
 }
 
-function mfa {
-  if [[ ${exit_script} == "true" ]]; then return; fi
-  # Check for valid aws-mfa session
-  unset expiration_date
 
-  # search long-term
-  # echo "!!! MFA: AWS_PROFILE: ${AWS_PROFILE}"
+convertTime() {
+  local input_time=$1
 
-  # Detecting source_profile to obtain mfa-device
-  source_profile=$(sed -n -e "/\[.*${AWS_PROFILE}\]/,/^$/ s/^[[:space:]]*source_profile[[:space:]]*=[[:space:]]*\(.*\)/\1/p" ${HOME}/.aws/config)
-  # echo "!!! MFA: source_profile: ${source_profile}"
-
-  if [[ -z ${source_profile} ]]; then
-    source_profile_longterm="${AWS_PROFILE}-long-term"
-    source_profile=${AWS_PROFILE}
+  if [[ $input_time =~ ^[0-9]+$ ]]; then
+    if [[ $(uname) == "Darwin" ]]; then
+      date -j -f "%s" $input_time +"%Y-%m-%d %H:%M:%S"
+    else
+      date -d @$input_time +"%Y-%m-%d %H:%M:%S"
+    fi
   else
-    source_profile_longterm="${source_profile}-long-term"
+    if [[ $(uname) == "Darwin" ]]; then
+      date -j -f "%Y-%m-%d %H:%M:%S" "$input_time" +"%s"
+    else
+      date -d "$input_time" +"%s"
+    fi
   fi
+}
 
-  expiration=$(sed -n -e "/\[$source_profile\]/,/^$/ s/^[[:space:]]*expiration[[:space:]]*=[[:space:]]*\(.*\)/\1/p" "$HOME/.aws/credentials")
 
-  osType=$(checkOS)
-  if [[ ${osType} == "macos" ]]; then
-    expiration_date=$(date -j -f "%Y-%m-%d %H:%M:%S" "${expiration}" "+%s" 2>/dev/null)
-    dateCmd="date -j -f "
-  elif [[ ${osType} == "linux" ]]; then
-    expiration_date=$(date -d "$expiration" +%s 2>/dev/null)
-  else
-    echo "!! ostype not linux or macos"
-  fi
 
-  #  expiration_date=$(${dateCmd}  "%Y-%m-%d %H:%M:%S" "$(sed -n -e "/\[${source_profile}\]/,/^$/ s/^[[:space:]]*expiration[[:space:]]*=[[:space:]]*\(.*\)/\1/p" ${HOME}/.aws/credentials)" "+%s" 2>/dev/null)
-  date_now=$(date +%s)
-  date_now_future=$((date_now + 1800))
-##  echo " DEBUG: $date_now_future" 
-  mfa_arn=$(sed -n -e "/\[${source_profile_longterm}\]/,/^$/ s/^[[:space:]]*aws_mfa_device[[:space:]]*=[[:space:]]*\(.*\)/\1/p" ${HOME}/.aws/credentials)
+function selectAWSProfile {
+  awsProfileGroups=$(jsonify-aws-dotfiles | jq -r '[.config[].group] | unique | sort | .[]' | grep -v null | gum choose --height 25)
+  selectedProfile=$(jsonify-aws-dotfiles | jq -r --arg group "$awsProfileGroups" '.config | to_entries | map(select(.value.group == $group)) | (["AWS ACCOUNT", "ROLE"] | @csv), (.[] | [.key, .value.role_arn] | @csv)' | gum table -w 40,120 --height 30)
+  selectedProfileName=$(echo "${selectedProfile}" | awk -F "," '{print $1}')
+  selectedProfileARN=$(echo "${selectedProfile}" | awk -F "," '{print $2}')
+  selectedProfileAccountID=$(echo "${selectedProfileARN}" | awk -F ":" '{print $5}')
 
-  if [[ ${expiration_date} -lt ${date_now_future} ]]; then
-    if [[ ! -z ${mfa_arn} ]]; then
-      echo aws-mfa --profile ${source_profile} --force --device ${mfa_arn}
+  if ! expr "${selectedProfileAccountID}" + 0 &>/dev/null; then echo "Error determing AccountID from ARN" ; fi
+  sourceProfile=$(jsonify-aws-dotfiles | jq -r --arg arn "$selectedProfileARN" ' .config | to_entries | map(select(.value.role_arn == $arn)) | .[0].value.source_profile // "error" ')
+}
+
+
+function setMFA {
+  awsMFADevice=$(awk -v profile="${sourceProfile}-long-term" ' $0 == "[" profile "]" {found=1; next} /^\[.*\]/ {found=0} found && /^aws_mfa_device/ {print $3; exit} ' ~/.aws/credentials)
+
+
+  if [[ -z ${currentMFASessionExpirationDate} ]]; then expiration="1" ;fi
+
+  #if [[ ${currentMFASessionExpirationDate} -lt ${date_now_future} ]]; then
+  if [[ ${currentMFASessionExpirationDate} -lt ${date_now} ]]; then
+    if [[ ! -z ${awsMFADevice} ]]; then
+      echo aws-mfa --profile ${sourceProfile} --force --device ${awsMFADevice}
       if [[ ! -z $totpScript ]]; then
-        mfacode=$(${totpScript})
-        echo ${mfacode} |  ${clipboardCommand}
-        echo "-- Copied to clipboard"; 
-        echo "${mfacode}"
-      else 
-        echo "Code: ${mfacode}"
-      fi
-      aws-mfa --profile ${source_profile} --force --device ${mfa_arn} 
-    else
-      echo "!! MFA_arn not found. Can't renew session"
-    fi
-  else
-    echo "MFA Valid, until: ${expiration}"
-
-fi
-}
-
-function read_selection {
-  PS3="Select a profile number: "
-  echo "-: Unset profile"
-  select item in "${profiles[@]}"; do
-    if [[ -n ${item} ]]; then
-      echo "AWS_PROFILE set to $item"
-      export AWS_PROFILE=${item}
-      break
-    fi
-    if [[ ${REPLY} == "-" ]]; then
-      echo "Unsetting profile"
-      unset AWS_PROFILE
-      if [[ $shell_type == "zsh" ]]; then
-        export PROMPT="$PROMPTBAK"
-        if [[ ${rprompt_config} == "true" ]]; then
-          unset RPROMPT
-        fi
+        totpCode=$(${totpScript})
+        echo ${totpCode} |  ${clipboardCommand}
+        echo "-- Copied to clipboard";
+        echo "${totpCode}"
       else
-        export PS1="$PS1BAK"
+        echo "Code: ${totpCode}"
       fi
-      exit_script=true
-      break
+      aws-mfa --profile ${sourceProfile} --force --device ${awsMFADevice}
+      if [[ $? -ne 0 ]]; then echo "!!  Error with AWS MFA code for device. Wrong TOPT?"; return;fi
     else
-      echo Invalid selection. Select a valid profile number or press ctrl+c to exit
+      echo "!! awsMFADevice not found. Can't renew session"
     fi
-  done
-}
-
-function check_sdk {
-  # Set AWS_SDK_LOAD_CONFIG to true to make this useful for tools such as Terraform and Serverless framework
-  if (($sdk == 1)); then
-    export AWS_SDK_LOAD_CONFIG=true
   else
-    export AWS_SDK_LOAD_CONFIG=false
+
+#    woutertest=$(convertTime "$currentMFASessionExpirationDate")
+
+    echo "Current MFA Session Valid, until: $(convertTime ${currentMFASessionExpirationDate})"
   fi
 }
 
-
-function list_config_profiles {
-config_profiles=($(grep -E '\[profile .+\]' ~/.aws/config | sed 's/\[profile \(.*\)\]/\1/'))
-header="Profile Name                 | Account Number | Region       | Role ARN"
-output="$header\n"
-osType=$(checkOS)
-
-
-for config_profile in "${config_profiles[@]}"; do
-
-
-if [[ ${osType} == "macos" ]]; then
-		profile_data=($(profile_name="${config_profile}"; awk -v profile="$config_profile" '$0 ~ "[profile " profile "]"{p=1} p && NF && !/^\[/{print; p=0}' ~/.aws/config))
-	else
-		profile_data=($(sed -n -e "/^\[profile $config_profile\]/,/^\[/ { /^\[/! p; }" ~/.aws/config | awk 'NF'))
-fi 
-
-
-
-  account_number=""
-  region=""
-  role_arn=""
-
-  for field in "${profile_data[@]}"; do
-    profkey=${field%%=*}
-    profvalue=${field#*=}
-
-    if [ "$profkey" = "role_arn" ]; then
-      account_number=$(echo "$profvalue" | awk -F ':' '{print $5}')
-      role_arn="$profvalue"
-    elif [ "$profkey" = "region" ]; then
-      region="$profvalue"
-    fi
-  done
-
-  output=$(printf "%-30s | %-15s | %-12s | %s\n" "$config_profile" "$account_number" "$region" "$role_arn")
-  echo $output
-  done | column -t -s '|'
-}
-
-if [ $# -gt 0 ]; then
-  while [ ! $# -eq 0 ]; do
-    case "$1" in
-    -l)
-      list_config_profiles
-      break
-      ;;
-    --help | -h)
-      usage
-      ;;
-    --no-sdk | -n)
-      sdk=0
-      # Kick off the main function:
-      main
-      ;;
-    *)
-      echo "Invalid script parameters"
-      main
-      ;;
-    esac
-    main
-  done
-else
-  # Kick off the main function:
-  main
-fi
+checkOS
+selectAWSProfile
+setDates
+setMFA
+export AWS_PROFILE=${selectedProfileName}
