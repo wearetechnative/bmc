8186b144e344976d5e7aec0da45ebe9d31cba5c3
TorreiroW <githubwto@toorren.net>
2024-09-09 11:59:36 +0200

    update

--- aws-profile-select.sh
+++ aws-profile-select.sh
@@ -4,6 +4,16 @@
 MFA=true
 SSO=""
 totpScript=""
+otpScript="rbw-menu.sh -t code -q new"
+#totpScript=""
+clipboardCommand="xclip -selection clipboard"
+
+
+
+# Initialize our own variables
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+verbose=0
+name=""
 
 
 function setDates {
@@ -56,21 +66,22 @@ convertTime() {
 
 
 function selectAWSProfile {
-  awsProfileGroups=$(jsonify-aws-dotfiles | jq -r '[.config[].group] | unique | sort | .[]' | grep -v null | gum choose --height 25)
-  selectedProfile=$(jsonify-aws-dotfiles | jq -r --arg group "$awsProfileGroups" '.config | to_entries | map(select(.value.group == $group)) | (["AWS ACCOUNT", "ROLE"] | @csv), (.[] | [.key, .value.role_arn] | @csv)' | gum table -w 40,120 --height 30)
+    awsProfileGroups=$(jsonify-aws-dotfiles | jq -r '[.config[].group] | unique | sort | .[]' | grep -v null | gum choose --height 25)
+    selectedProfile=$(jsonify-aws-dotfiles | jq -r --arg group "$awsProfileGroups" '.config | to_entries | map(select(.value.group == $group)) | (["AWS ACCOUNT", "ROLE"] | @csv), (.[] | [.key, .value.role_arn] | @csv)' | gum table -w 40,120 --height 30)
   selectedProfileName=$(echo "${selectedProfile}" | awk -F "," '{print $1}')
   selectedProfileARN=$(echo "${selectedProfile}" | awk -F "," '{print $2}')
   selectedProfileAccountID=$(echo "${selectedProfileARN}" | awk -F ":" '{print $5}')
 
-  if ! expr "${selectedProfileAccountID}" + 0 &>/dev/null; then echo "Error determing AccountID from ARN" ; fi
-  sourceProfile=$(jsonify-aws-dotfiles | jq -r --arg arn "$selectedProfileARN" ' .config | to_entries | map(select(.value.role_arn == $arn)) | .[0].value.source_profile // "error" ')
+#  if ! expr "${selectedProfileAccountID}" + 0 &>/dev/null; then echo "Error determing AccountID from ARN" ; fi
+
+sourceProfile=$(jsonify-aws-dotfiles | jq -r --arg arn "$selectedProfileARN" ' .config | to_entries | map(select(.value.role_arn == $arn)) | .[0].value.source_profile // "Error" ')
+
+if [[ ${sourceProfile} == "Error" ]]; then sourceProfile=${selectedProfileName}; fi
 }
 
 
 function setMFA {
   awsMFADevice=$(awk -v profile="${sourceProfile}-long-term" ' $0 == "[" profile "]" {found=1; next} /^\[.*\]/ {found=0} found && /^aws_mfa_device/ {print $3; exit} ' ~/.aws/credentials)
-
-
   if [[ -z ${currentMFASessionExpirationDate} ]]; then expiration="1" ;fi
 
   #if [[ ${currentMFASessionExpirationDate} -lt ${date_now_future} ]]; then
@@ -85,7 +96,7 @@ function setMFA {
       else
         echo "Code: ${totpCode}"
       fi
-      aws-mfa --profile ${sourceProfile} --force --device ${awsMFADevice}
+      aws-mfa --profile ${sourceProfile} --force --device ${awsMFADevice} 
       if [[ $? -ne 0 ]]; then echo "!!  Error with AWS MFA code for device. Wrong TOPT?"; return;fi
     else
       echo "!! awsMFADevice not found. Can't renew session"
@@ -97,6 +108,7 @@ function setMFA {
     echo "Current MFA Session Valid, until: $(convertTime ${currentMFASessionExpirationDate})"
   fi
 }
+
 
 checkOS
 selectAWSProfile
